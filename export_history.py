6f4d87dee52d1e0861a6fd19529dc9ec3527ef1a
Joe Reddington <joe@joereddington.com>
2018-11-07 16:50:09 +0000

    seems to be working fine

--- export_history.py
+++ export_history.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 
 def get_cursor(url):
@@ -14,12 +15,45 @@ def get_all():
 def filter_by_date(matches, text):
     return_me=[]
     for row in matches:
-        if text in row[0]:
+        if text in str(row[0]):
             return_me.append(row)
     return return_me
             
 
+def privacy_morph(row, text,newtext=None):
+    if newtext==None:
+        newtext=text
+    if text in row[1]:
+        return (row[0], newtext)
+    else:
+        return row
+
+
+
+def privacy_filter(matches):
+    return_me=[]
+    for row in matches: 
+            newrow=row
+            newrow=privacy_morph(newrow,"https://www.linkedin.com")
+            newrow=privacy_morph(newrow,"http://designs.theopenvoicefactory.org")
+            newrow=privacy_morph(newrow,"https://www.facebook.com")
+            newrow=privacy_morph(newrow,"https://www.dropbox.com")
+            newrow=privacy_morph(newrow,"https://accounts.google.com")
+            newrow=privacy_morph(newrow,"https://mail.google.com")
+            return_me.append(newrow)
+    
+    return return_me
+
 if __name__=="__main__":
-    for row in get_all():
-        print row
+    with open("index.html","w") as html_file:
+        number_of_days_to_go_back=10
+        for i in range(number_of_days_to_go_back+1):
+                date= str(datetime.date.today()-datetime.timedelta(number_of_days_to_go_back-i))
+                html_file.write("<H2>{}<H2><br>".format(date))
+                html_file.write("<ul>")
+                for row in filter_by_date(privacy_filter(get_all()),date):
+                    html_file.write("<li> {}, {}".format(row[0][11:-3],row[1]))
+                html_file.write("</ul>")
+
+
 
