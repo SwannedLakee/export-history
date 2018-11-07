ee5b6f78f85a1eb9e01b0b041e6b986d894f7d26
Joe Reddington <joe@joereddington.com>
2018-11-07 12:27:13 +0000

    Now filters by day

--- export_history.py
+++ export_history.py
@@ -4,9 +4,22 @@ import sqlite3
 def get_cursor(url):
     return sqlite3.connect(url).cursor()
 
-db = sqlite3.connect('/Users/josephreddington/Library/Application Support/Firefox/Profiles/datm9h3r.default-1541155689776/places.sqlite')
-cursor=db.cursor()
-cursor.execute('''SELECT datetime(moz_historyvisits.visit_date/1000000,'unixepoch'), moz_places.url FROM moz_places, moz_historyvisits WHERE moz_places.id = moz_historyvisits.place_id''')
-print "here"
-for row in cursor:
-    print row
+
+def get_all():
+    cursor = sqlite3.connect('/Users/josephreddington/Library/Application Support/Firefox/Profiles/datm9h3r.default-1541155689776/places.sqlite').cursor()
+    cursor.execute('''SELECT datetime(moz_historyvisits.visit_date/1000000,'unixepoch'), moz_places.url FROM moz_places, moz_historyvisits WHERE moz_places.id = moz_historyvisits.place_id''')
+    return cursor.fetchall()
+
+
+def filter_by_date(matches, text):
+    return_me=[]
+    for row in matches:
+        if text in row[0]:
+            return_me.append(row)
+    return return_me
+            
+
+if __name__=="__main__":
+    for row in get_all():
+        print row
+
