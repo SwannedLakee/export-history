03b1fd4d2806b3c725027fb4931ef179f85e0a27
Joseph Reddington <joe@joereddington.com>
2020-09-02 13:13:46 +0100

    starting to be able to add log.md notes

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
-echo "We are here in history"
-echo "We are here in history">log.txt
-
+cd ../todo.txt/
+git log | grep "##" > ../history/logs.md 
+cd -
 python export_history.py
 
 rm *-shm *-wal
