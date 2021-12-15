87f698bfccaa6cc4af81786c9acee8722dc7dca9
Joe Reddington <joe2021@Josephs-Mini.fritz.box>
2021-12-15 14:44:44 +0000

    improving deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 python3 export_history.py
-copy history.html index.html
+cat head.html > index.html
+cat history.html >> index.html
 git commit -a -m "Update"
 git push 
