cbe96363d802f6e21c1f0f5575ba92c9826d5d74
Joseph Reddington <joe@joereddington.com>
2022-02-03 13:45:02 +0000

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cp /Users/joe2021/Library/Application\ Support/Firefox/Profiles/dato3gap.default-release/places.sqlite databases/firefox.sqlite
-python3 export_history.py
+python3 export_history.py >log.txt
 cat head.html > index.html
 cat history.html >> index.html
 git commit -a -m "Update"
