db5e4eb2418737912f27daf45ad108a11cd3d7da
Joseph Reddington <joe@joereddington.com>
2022-02-04 12:13:24 +0000

    Removed logging from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cp /Users/joe2021/Library/Application\ Support/Firefox/Profiles/dato3gap.default-release/places.sqlite databases/firefox.sqlite
-python3 export_history.py >log.txt
+python3 export_history.py 
 cat head.html > index.html
 cat history.html >> index.html
 git commit -a -m "Update"
