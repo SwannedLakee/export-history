a590caffa67c4ee290cd612eeb434c3868f3933f
Joseph Reddington <joe@joereddington.com>
2022-04-25 10:50:16 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 cp /Users/joe2021/Library/Application\ Support/Firefox/Profiles/dato3gap.default-release/places.sqlite databases/firefox.sqlite
 python3 export_history.py 
 cat head.html > index.html
-cat head.html > smartsocial.html
+cat socialhead.html > smartsocial.html
 cat history.html >> index.html
 cat social.html >> smartsocial.html
 git commit -a -m "Update"
