e81ec9c1fd6952aa5aa07c97f3bf03c3d37dd730
Joseph Reddington <joe@joereddington.com>
2022-08-21 15:20:25 +0100

    Update

--- test_export_history.py
+++ test_export_history.py
@@ -9,17 +9,17 @@ def get_content(infilename):
 class historyTest(unittest.TestCase):
 
     def test_hello(self):
-        data=sorted(export_history.get_history_from_database('databases/firefox.sqlite','firefox'))
+        data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
         print(data[1])
         self.assertTrue("14:52" in data[1][0])
 
     def test_first_milseconds(self):
-        data=sorted(export_history.get_history_from_database('databases/firefox.sqlite','firefox'))
+        data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
         print(data[1])
         self.assertEqual(1482159174330000,data[1][3])
 
     def test_todays_internet(self):
-        data=export_history.get_history_from_database('databases/firefox.sqlite','firefox', 1614334237000000)
+        data=export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox', 1614334237000000)
         for record in data:
             print(record)
 
