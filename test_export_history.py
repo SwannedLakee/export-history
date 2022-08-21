7466972a366c42ba1fd9415a2ff7b59823cc504c
Joseph Reddington <joe@joereddington.com>
2022-08-21 15:25:20 +0100

    Updating the tests

--- test_export_history.py
+++ test_export_history.py
@@ -10,18 +10,12 @@ class historyTest(unittest.TestCase):
 
     def test_hello(self):
         data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
-        print(data[1])
-        self.assertTrue("14:52" in data[1][0])
+        self.assertTrue("14:14" in data[0][0])
 
     def test_first_milseconds(self):
         data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
-        print(data[1])
-        self.assertEqual(1482159174330000,data[1][3])
+        self.assertEqual(1661091271297047,data[0][3])
 
-    def test_todays_internet(self):
-        data=export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox', 1614334237000000)
-        for record in data:
-            print(record)
 
 if __name__=="__main__":
     unittest.main()
