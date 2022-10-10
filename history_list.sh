4f8d03678d2cde6177ef8d0c17cf736025dabd7a
Joe Reddington <joe@joereddington.com>
2022-10-10 13:01:37 +0100

    That script works

--- history_list.sh
+++ history_list.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-#cp /Users/joe2021/Library/Application\ Support/Firefox/Profiles/qrl7jda1.default-release/places.sqlite databases/firefox.sqlite
-cp /Users/joe2021/Library/Application\ Support/Firefox/Profiles/dato3gap.default-release/places.sqlite databases/firefox.sqlite
-python3 history_list.py "$1"
+cp /Users/joe/Library/Application\ Support/Firefox/Profiles/er7tx3r3.default-release/places.sqlite databases/firefox.sqlite 
+python3 export_history.py 
