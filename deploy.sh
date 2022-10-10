142f45a4ed080d18f606c4b59ff78e1dfc7727c5
Joe Reddington <joe@joereddington.com>
2022-10-10 13:17:55 +0100

    updating deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cp /Users/joe2021/Library/Application\ Support/Firefox/Profiles/dato3gap.default-release/places.sqlite databases/firefox.sqlite
+cp /Users/joe/Library/Application\ Support/Firefox/Profiles/er7tx3r3.default-release/places.sqlite databases/firefox.sqlite
 python3 export_history.py 
 cat head.html > index.html
-cat socialhead.html > smartsocial.html
 cat history.html >> index.html
-cat social.html >> smartsocial.html
 git commit -a -m "Update"
 git push 
