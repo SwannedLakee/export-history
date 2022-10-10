e1be2e7d1d712e8d9c236fd4b82a392cd0cbb865
Joe Reddington <joe@joereddington.com>
2022-10-10 12:50:56 +0100

    tests passing

--- test_export_history.py
+++ test_export_history.py
@@ -9,18 +9,17 @@ def get_content(infilename):
 class historyTest(unittest.TestCase):
 
     def test_hello(self):
-        data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
+        data=sorted(export_history.get_history_from_database('1k2elc2y.Test/places.sqlite','firefox'))
         print(data)
-        self.assertTrue("08:15" in data[0][0])
+        self.assertTrue("11:35" in data[0][0])
 
     def test_first_milseconds(self):
-        data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
-        self.assertEqual(1535962538426000,data[0][3])
-        self.assertTrue("14:14" in data[0][0])
+        data=sorted(export_history.get_history_from_database('1k2elc2y.Test/places.sqlite','firefox'))
+        self.assertEqual(1665401723897728,data[0][3])
 
-    def test_first_milseconds(self):
-        data=sorted(export_history.get_history_from_database('testinputs/testprofile/places.sqlite','firefox'))
-        self.assertEqual(1661091271297047,data[0][3])
+    def test_first_milseconds2(self):
+        data=sorted(export_history.get_history_from_database('1k2elc2y.Test/places.sqlite','firefox'))
+        self.assertEqual(1665401723897728,data[0][3])
 
 
 if __name__=="__main__":
