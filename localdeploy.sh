c45ea8c206087f2296cce172679b13e632f038c2
Joe Reddington <joe.reddington@gmail.com>
2023-01-13 15:05:31 +0000

    Adding a local deploy for testing

---
+++ localdeploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cp /Users/joe/Library/Application\ Support/Firefox/Profiles/er7tx3r3.default-release/places.sqlite databases/firefox.sqlite
+python3 export_history.py 
+cat head.html > localindex.html
+cat history.html >> localindex.html
